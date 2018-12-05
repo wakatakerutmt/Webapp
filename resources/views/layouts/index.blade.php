8ff849281c0fa287d32990b63a37a11b4fa258e8
Takuya Iida <wakatakerutmt@ezweb.ne.jp>
2018-12-05 23:10:22 +0900

    indexのビュー作成(途中)

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -5,18 +5,34 @@
   <title>Like Wntaledy</title>
   <link rel="icon" href="">
   <meta name="description" content="wantedly風サイト">
+
+  <!-- bootstrap -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="css/user.css">
+
 </head>
 
 <body>
 
-  <header class="header-menu">
+  <header class="header">
+      <nav class="navigation">
+          <ul>
+              <li class="nav-left"><a href="">Like Wantedly</a></li>
+              <li>ログイン</li>
+              <li>採用担当の方</li>
+              <li>iOS/Androidアプリ</li>
+          </ul>
+      </nav>
   </header>
 
-  <div class="top contents">
-    @yield('content')
-  </div>
 
-  <footer class="footter-menu">
+    @yield('content')
+    
+
+  <footer class="footer">
+      <div class="footer-top"></div>
+      <div class="footer-link"></div>
   </footer>
 
 </body>
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.index')
- @section('content')
- indexページ
+@section('content')
+
+<div class="contents">
+<!--
+ indexページ()
  @if (Auth::check())
  <p> ユーザー名: {{ $user->name }} , メール： {{ $user->email }} ,
    <form action="/logout" method="POST">
@@ -10,10 +13,49 @@
     <a href="/profile">プロフィール</a>
 @else
  <a href="/login">ログイン</a> | <a href="/signup">登録</a>
-@endif
+@endif -->
 
-@php
-  print_r($user);
-@endphp
+    <section class="main">
+      <div class="main-message">
+        <h1>「はたらく」をより面白く</h1>
+        <p>ウォンテッドリー風SNSです。</p>
+      </div>
+
+      <div class="main-register-form">
+          <div class="main-facebook-register">FaceBookで今すぐログイン</div>
+          <div class="main-email-register">Emailで登録</div>
+      </div>
+
+      <div class="main-next-section-nav">MORE ABOUT WANTEDLY</div>
+    </section>
+
+
+    <section class="more-info">
+        <h2>共感や人とつながり、シゴトでココロおどろう。無料です。</h2>
+        <ul>
+          <li>
+            <!-- <img> -->
+            <h3>であう</h3>
+            <p>キラっと光るプロジェクトや会社を見つけて、気軽に訪問しよう</p>
+          </li>
+          <li>
+            <!-- <img> -->
+            <h3>つながる</h3>
+            <p>キャリアプロフィールを作成<br/>
+            　　人脈を構築し、広げよう
+            </p>
+          </li>
+          <li>
+            <!-- <img> -->
+            <h3>つくる</h3>
+            <p>
+               ビジネス専用グループチャット<br/>
+               Wantedly Chat で生産性を倍に
+            </p>
+          </li>
+    </section>
+
+
+</div>
 
 @endsection
